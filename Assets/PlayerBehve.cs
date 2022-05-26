6646a76ce0e663d53b9a6c6d45f8ad1d80a90b95
Fran-Godoy <101577372+Fran-Godoy@users.noreply.github.com>
2022-05-26 09:29:14 -0300

    Initial commit

---
+++ Assets/PlayerBehve.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehve : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject Clon;
+            Clon = Instantiate(prefab);
+            Clon.transform.position = spawnPoint.transform.position -Vector3.forward;
+        }   
+    }
+}
